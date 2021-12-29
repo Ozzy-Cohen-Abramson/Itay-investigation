96076897b4bcea6aa5b379efa9a8f795a06bf691
Ozzy Cohen Abramson <odentionofficial@gmail.com>
2021-12-29 08:55:12 +0200

    commented out google maps

--- src/Components/Footer/Footer.js
+++ src/Components/Footer/Footer.js
@@ -8,45 +8,45 @@ import "./footer.css";
 
 const FooterPage = () => {
   return (
-    <MDBFooter className='pt-4 footer'>
-      <MDBContainer fluid className='text-center text-md-left'>
+    <MDBFooter className="pt-4 footer">
+      <MDBContainer fluid className="text-center text-md-left">
         <MDBRow>
-          <MDBCol md='3' className='footer-collumn'>
-            <h3 className='title '>מפת האתר</h3>
-            <ul className='footer-list'>
+          <MDBCol md="4" className="footer-collumn">
+            <h3 className="title ">מפת האתר</h3>
+            <ul className="footer-list">
               <li>
-                <a as={Link} href='/'>
+                <a as={Link} href="/">
                   דף הבית
                 </a>
               </li>
-              <li className='list-unstyled'>
-                <a as={Link} href='/about'>
+              <li className="list-unstyled">
+                <a as={Link} href="/about">
                   אודות
                 </a>
               </li>
-              <li className='list-unstyled'>
-                <a as={Link} href='/services'>
+              <li className="list-unstyled">
+                <a as={Link} href="/services">
                   תחומי עיסוק
                 </a>
               </li>
-              <li className='list-unstyled'>
-                <a as={Link} href='/contact'>
+              <li className="list-unstyled">
+                <a as={Link} href="/contact">
                   צרו קשר
                 </a>
               </li>
             </ul>
           </MDBCol>
-          <MDBCol md='3' className='footer-collumn'>
-            <h3 className='title'>פרטים נוספים</h3>
-            <ul className='footer-list'>
+          <MDBCol md="4" className="footer-collumn">
+            <h3 className="title">פרטים נוספים</h3>
+            <ul className="footer-list">
               <li>טלפון: 053-7865001</li>
             </ul>
-            <h4 className='small-title'>שעות פעילות</h4>
-            <ul className='footer-list'>
+            <h4 className="small-title">שעות פעילות</h4>
+            <ul className="footer-list">
               <li>שבעה ימים בשבוע 24 שעות ביממה.</li>
             </ul>
           </MDBCol>{" "}
-          <MDBCol md='3' className='footer-collumn'>
+          {/* <MDBCol md='3' className='footer-collumn'>
             <h3 className='title'>מיקומנו</h3>
 
             <iframe
@@ -55,19 +55,20 @@ const FooterPage = () => {
               width='300'
               height='300'
               loading='lazy'></iframe>
-          </MDBCol>
-          <MDBCol md='3' className='footer-collumn'>
-            <h3 className='title'>צור קשר</h3>
+          </MDBCol> */}
+          <MDBCol md="4" className="footer-collumn">
+            <h3 className="title">צור קשר</h3>
             <ContactForm />
           </MDBCol>
         </MDBRow>
       </MDBContainer>
-      <div className='footer-copyright text-center py-3'>
+      <div className="footer-copyright text-center py-3">
         <MDBContainer fluid>
           &copy; {new Date().getFullYear()} Copyright:{" "}
           <a
-            href='https://ozzy-cohen-abramson.github.io/porfolio/'
-            target='_blank'>
+            href="https://ozzy-cohen-abramson.github.io/porfolio/"
+            target="_blank"
+          >
             Oz Cohen Abramson
           </a>
         </MDBContainer>
